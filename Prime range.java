ff6ad9a644e350cd26438e4d813e8d39c5ca6d12
Arpan Chowdhury <arpanchowdhury003@gmail.com>
2024-09-01 22:14:34 +0530

    Create Prime range.java

---
+++ Prime range.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class PrimeRangeChecker {
+
+    
+    public static boolean isPrime(int number) {
+        if (number <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+   
+    public static void printPrimesInRange(int start, int end) {
+        System.out.println("Prime numbers between " + start + " and " + end + " are:");
+        for (int i = start; i <= end; i++) {
+            if (isPrime(i)) {
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println(); 
+    }
+
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+
+        
+        System.out.print("Enter the start of the range: ");
+        int start = scanner.nextInt();
+
+        System.out.print("Enter the end of the range: ");
+        int end = scanner.nextInt();
+
+        
+        printPrimesInRange(start, end);
+
+        scanner.close();  
+    }
+}
